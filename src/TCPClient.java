0b42d8d50d9924801609427e1bcccf90c03769a4
vinjour <jourdren.vincent@gmail.com>
2022-11-18 16:55:45 +0100

    TCP server and client

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,2 +1,46 @@
+import java.net.*;
+import java.io.*;
+
+
 public class TCPClient {
+
+    public static void main(String[] args) {
+        if (args.length < 2) return;
+
+        String hostname = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        try (Socket socket = new Socket(hostname, port)) {
+
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+
+            Console console = System.console();
+            String text;
+
+            do {
+                text = console.readLine("Enter text: ");
+
+                writer.println(text);
+
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                String time = reader.readLine();
+
+                System.out.println(time);
+
+            } while (!text.equals("bye"));
+
+            socket.close();
+
+        } catch (UnknownHostException ex) {
+
+            System.out.println("Server not found: " + ex.getMessage());
+
+        } catch (IOException ex) {
+
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+    }
 }
--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,2 +1,42 @@
+import java.io.*;
+import java.net.*;
+
+
 public class TCPServer {
+
+    public static void main(String[] args) {
+
+        int port = Integer.parseInt(args[0]);
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+            System.out.println("Server is listening on port " + port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected");
+
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+
+
+                String text;
+
+                do {
+                    text = reader.readLine();
+                    writer.println("Server: " + text);
+
+                } while (!text.equals("bye"));
+
+                socket.close();
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
 }
